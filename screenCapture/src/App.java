9f223233b245063815456a29062d71686b270bab
lucky634634 <khoa21102003@gmail.com>
2022-12-23 09:18:12 +0700

    Delete unnecessary

--- screenCapture/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-
-    public static void main(String[] args) throws Exception {
-        mainFrame frame = new mainFrame();
-        frame.insertPrtScrButton(100, 100);
-        frame.insertPrtScrButton(200, 200);
-    }
-}
